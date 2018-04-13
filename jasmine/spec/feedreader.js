b013791aac1e66265d5dd8b3011f229022148f76
gallasmur <jrabal.segura@gmail.com>
2018-04-13 17:52:26 +0200

    Add test to check if meny change visibility. Punto 12

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -63,14 +63,20 @@ $(function() {
         it('element is hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
-    });
-        
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+         it('changes visibility when the menu icon is clicked', function() {
+             $('.menu-icon-link').click();
+             expect($('body').hasClass('menu-hidden')).toBe(false);
+
+             $('.menu-icon-link').click();
+             expect($('body').hasClass('menu-hidden')).toBe(true);
+         })
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
